70fc9b88e44823821c5513d66010ec915ceee275
Vishakumar123 <visalkumar12123@gmail.com>
2024-09-04 08:11:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+
+    <header>
+        <h1>Enter number below and press sumbit</h1>
+    </header>
+
+   
+    </nav>
+ 
+<h2><label for="username">Number:</label></h2> <input type="text" id="username" name="username"> 
+
+<br>
+<br>
+
+<button type="button" >sumbit</button>
+ 
+
+    <section id="home">
+        <h2>Current number</h2>
+           <h3>current number</h>
+
+   
+
+</body>
+</html>
